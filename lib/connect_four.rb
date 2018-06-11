65a68d4175f121a0d82a1823e1481ea569ba83ad
Romane <romane71193@gmail.com>
2018-06-11 00:23:53 -0400

    add tests/logic

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,8 +1,67 @@
 
 class Connectfour
+attr_accessor :board,:turn
+
+def initialize
+@board = Array.new(42,' ')
+@player = '_'
+@turn = 0
+end
 
 def play
-puts 'hi'
+
+take_turn
 end
+
+def game_state
+  puts
+  puts "\t \t          Welcome to Connectfour"
+  puts
+  puts "\t \t \t #{@board[0]} | #{@board[1]} | #{@board[2]} | #{@board[3]} | #{@board[4]} | #{@board[5]} | #{@board[6]}"
+  puts " \t \t  \t -------------------------"
+  puts "\t \t \t #{@board[7]} | #{@board[8]} | #{@board[9]} | #{@board[10]} | #{@board[11]} | #{@board[12]} | #{@board[13]}"
+  puts "\t \t \t -------------------------"
+  puts "\t \t \t #{@board[14]} | #{@board[15]} | #{@board[16]} | #{@board[17]} | #{@board[18]} | #{@board[19]} | #{@board[20]}"
+  puts "\t \t \t -------------------------"
+  puts "\t \t \t #{@board[21]} | #{@board[22]} | #{@board[23]} | #{@board[24]} | #{@board[25]} | #{@board[26]} | #{@board[27]}"
+  puts "\t \t \t -------------------------"
+  puts "\t \t \t #{@board[28]} | #{@board[29]} | #{@board[30]} | #{@board[31]} | #{@board[32]} | #{@board[33]} | #{@board[34]}"
+  puts "\t \t \t -------------------------"
+  puts "\t \t \t #{@board[35]} | #{@board[36]} | #{@board[37]} | #{@board[38]} | #{@board[39]} | #{@board[40]} | #{@board[41]}"
+  puts
+end
+
+def take_turn
+  until turn == 42
+  game_state
+
+  turn.even? ?  player = '⏺' : player = '○'
+  puts "\t \t Player #{player} Enter a square (1-42)"
+  input = gets.chomp.to_i - 1
+
+  if input > 43 || input == -1
+  puts "\t \t Invaid,please enter a number between 1 and 42"
+
+elsif board[input] == '⏺' || board[input] == '○'
+  puts "\t \t Square has already been taken!"
+
+  else
+  board[input] = "#{player}"
+  @turn += 1
+  draw
+  end
+  end
+
+end
+
+def draw
+  if @turn == 42
+  p "Game Over! Draw! Nobody Won!"
+  exit
+  else
+    false
+  end
+end
+
 
 end
---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+require 'connect_four.rb'
+
+describe Connectfour do
+it 'makes sure Connectfour is a class' do
+  expect(Connectfour.new).to_not be_nil
+end
+end
+
+describe Connectfour do
+it 'generates board with 42 empty slots' do
+ g =  Connectfour.new
+ g.instance_variable_get(:@board)
+  expect(g.instance_variable_get(:@board)).to eq  Array.new(42," ")
+end
+end
+
+describe Connectfour do
+it 'Establishes player one and player two' do
+ g =  Connectfour.new
+ g.instance_variable_get(:@player)
+  expect(g.instance_variable_get(:@player)).to eq '_'
+end
+end
+
+
+describe Connectfour do
+it 'Tracks turns' do
+ g =  Connectfour.new
+ g.instance_variable_get(:@turn)
+  expect(g.instance_variable_get(:@turn)).to eq 0
+end
+end
+
+describe '#turn' do
+it 'increments turns' do
+ g =  Connectfour.new
+g.turn = 22
+ g.instance_variable_get(:@turn)
+  expect(g.instance_variable_get(:@turn)).to eq 22
+end
+end
+
+
+describe '#draw' do
+it 'returns false if not a draw' do
+ g =  Connectfour.new
+ g.turn = 22
+ g =  Connectfour.new.draw
+  expect(g).to eq false
+end
+end
+
+describe '#win' do
+it 'lets a player know if he or she has won' do
+ g =  Connectfour.new
+ g.turn = 22
+ g =  Connectfour.new.draw
+  expect(g).to eq false
+end
+end
